4db41e1f640b9f80ba69eecb876fa4a58c6dc977
Riku Lauttia <riku.lauttia@gmail.com>
2025-01-31 19:05:27 +0200

    exercise 11.9 completed

--- playwright.config.js
+++ playwright.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   webServer: {
     command: 'npm run start',
-    port: 3001,
+    url: 'http://localhost:8080',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
-    baseURL: 'http://localhost:3001/',
+    baseURL: 'http://localhost:8080/',
   },
 })
